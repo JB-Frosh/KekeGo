31b0133ae4af1bf0a484fe7a64144f9b1334ed95
Jimoh <jimohbasit521@gmail.com>
2026-09-15 00:08:39 -0700

    Add API health check endpoint

--- KekeGo/urls.py
+++ KekeGo/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from common.views import health_check
 
 urlpatterns = [
+    path('', health_check, name='health-check'),
     path('admin/', admin.site.urls),
     path('api/auth/', include('accounts.urls')),
     path('api/locations/', include('locations.urls')),
